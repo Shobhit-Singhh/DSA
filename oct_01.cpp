7871240513e0a6b8a0b0f6292cfab21ece2bcdaf
Shobhit <shobhit.singh@iitg.ac.in>
2022-10-01 09:29:31 +0530

    oct_01_commit2

--- oct_01.cpp
+++ oct_01.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+//reversing a number
+//T stands for tail recursion
 int T_rev(int n, int count = 0)
 {
     if (n == 0)
@@ -13,7 +15,7 @@ int main()
 system("clear");
 int n;
 cout<<"enter the number";
-cin>>n;
+cin>>n;         // user input
 cout<<T_rev(n);
 return 0;
 }
