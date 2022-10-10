6b566e5eb13f796a543385e1a7ffd94a3f003f29
Shobhit <shobhit.singh@iitg.ac.in>
2022-10-10 13:49:39 +0530

    oct_10_commit1

---
+++ oct_10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+// calculating sum of all digit of a number
+int sum_of_digits(int n, int m=0)
+{  if (n <= 0)
+        return m;
+return sum_of_digits(n/10,(m+(n%10)));
+}
+int main()
+{
+    system("clear");
+    int n1;
+    cout << "enter the numbers\n";
+    cin >> n1;
+    cout<<sum_of_digits(n1);
+    return 0;
+}
+
