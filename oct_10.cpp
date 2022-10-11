03a96bd4964858310dfe35b2c37caae5b2938c49
Shobhit <shobhit.singh@iitg.ac.in>
2022-10-11 07:45:03 +0530

    oct_11_commit1

--- oct_10.cpp
+++ oct_10.cpp
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 using namespace std;
 // calculating sum of all digit of a number
-int sum_of_digits(int n, int m=0)
-{  if (n <= 0)
+int sum_of_digits(int n, int m = 0)
+{
+    if (n <= 0)
         return m;
-return sum_of_digits(n/10,(m+(n%10)));
+    return sum_of_digits(n / 10, (m + (n % 10)));
 }
+
 int main()
 {
     system("clear");
     int n1;
     cout << "enter the numbers\n";
     cin >> n1;
-    cout<<sum_of_digits(n1);
+    cout << sum_of_digits(n1);
     return 0;
 }
-
