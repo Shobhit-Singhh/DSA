442f80a7140d7e9a11abc6d3fa5a33b5d125bd30
Shobhit <shobhit.singh@iitg.ac.in>
2022-11-01 11:30:19 +0530

    11_01_commit

---
+++ 11_1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+// maximum of a sub array
+int main()
+{
+    system("clear");
+
+    int n;
+    cout << "enter the array length\n";
+    cin >> n;
+    int arr[n];
+    cout << "enter the array\n";
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "\n\n array :--\n";
+
+    for (int i = 0; i < n; i++)
+        cout << "|" << arr[i];
+    int current = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        current = current + arr[i];
+        if (current > arr[i])
+            arr[i] = current;
+        else
+            current = arr[i];
+    }
+
+    current = 0;
+    cout << "\n\n updated array :--\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "|" << arr[i];
+        if (current < arr[i])
+            current = arr[i];
+    }
+
+    cout << "\n\n max sum of sub array :- " << current;
+    return 0;
+}
