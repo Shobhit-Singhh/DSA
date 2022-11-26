5c6111757ef6e8527a885e1915175a3fc0d49e3f
Shobhit <shobhit.singh@iitg.ac.in>
2022-11-27 04:27:33 +0530

    reversing words in the string

---
+++ 11_29.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    system("clear");
+    string str ;
+    cout<<"enter the string\n";
+    getline(cin,str);
+    int j = 0, i = 0, p = 0;
+    int start = 0;
+    for (; i <= str.length(); i++)
+    { 
+        p=i;
+        if (str[i] == ' '||i==str.length())
+        {   i--;
+            while (j <= i)
+            {
+                swap(str[j], str[i]);
+                j++;
+                i--;
+            }
+            i = p;
+            j = p+1;
+        }
+    }
+for(int i=str.length();i>=0;i--)
+    cout<<str[i];
+
+return 0;
+}
