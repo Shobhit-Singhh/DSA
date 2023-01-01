b9a6f1c93f2789f0cd84ce9725bb08ae1b6fcb70
Shobhit <shobhit.singh@iitg.ac.in>
2023-01-01 13:10:23 +0530

    every thing starts from here

---
+++ 01_01.cpp
@@ -0,0 +1,31 @@
+ 
+
+//Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to 
+target.
+
+//You may assume that each input would have exactly one 
+solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    { vector<int> v;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums.size();j++)
+            {if(i==j)
+            continue;
+            if(nums[i]+nums[j]==target)
+                {v.push_back(i);
+                v.push_back(j);
+                return v;
+                break;
+                }}
+        }return v;
+    }
+
+};
+
